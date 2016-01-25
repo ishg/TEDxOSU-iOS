5d7b4f82c8e4de9fde8ea3f25362d9c03f670161
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-01-25 00:28:31 -0500

    Added Speaker View and Navigation

---
+++ TEDxOhioStateUniversity/SpeakerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SpeakerViewController.swift
+//  TEDxOhioStateUniversity
+//
+//  Created by Ishmeet Grewal on 1/25/16.
+//  Copyright © 2016 Ishmeet Grewal. All rights reserved.
+//
+
+import UIKit
+
+class SpeakerViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
