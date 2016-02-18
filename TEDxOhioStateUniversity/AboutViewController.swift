1aa40ba75c201db2a8135543f3e64333ea8815e2
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2016-02-18 12:25:12 -0500

    About page added static content. Started Team List

--- TEDxOhioStateUniversity/AboutViewController.swift
+++ TEDxOhioStateUniversity/AboutViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 import Static
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var BannerImageHeightConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var tedLabel: UILabel!
+    @IBOutlet weak var tedxLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     
-    
+    var items: [String] = ["We", "Heart", "Swift"]
     
     
     required init?(coder aDecoder: NSCoder) {
@@ -35,9 +39,14 @@ class AboutViewController: UIViewController {
         //tabBarItem.image = tabBarItem.image.imageWithColor(UIColor.yellowColor()).imageWithRenderingMode(.AlwaysOriginal)
         
         
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "teamCell")
+        
         let width = self.view.frame.size.width
         self.BannerImageHeightConstraint.constant = width/3575 * 500
         self.headlineLabel.text = "TEDxOhioStateUniversity is a student organization at Ohio State founded in 2011 and comprises of mainly undergraduate students and university faculty. It is locally funded by various sponsors at Ohio State and within Columbus. Student members organize an annual event comprising of TED Talk videos and live speakers from the greater Ohio State University community."
+        self.messageLabel.text = "I am more than thrilled to see what this upcoming year has in store for TEDxOhioStateUniversity! After four years on campus, we are ready to take the TEDx experience to a new level this March 5, 2016 at the Mershon Auditorium. While we have many new and exciting things lined up for the year ahead, our organization still works under the mission of cultivating an event for students and city leaders who are passionate about challenging ideas and inspiring others. With this mission in mind, our team will work tirelessly to make this year’s event the best and most innovative the university has yet witnessed. I am confident that with the help of our outstanding partners, we will achieve these goals, allowing Ohio State University to join an international community of thinkers and doers. I could not be prouder to be a part of such an ambitious group of individuals, and I hope that you will join us along this exciting journey as we inspire OSU one idea worth spreading at a time."
+        self.tedLabel.text = "TED is a nonprofit organization devoted to \"ideas worth spreading\"."
+        self.tedxLabel.text = "In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience."
         
         // Do any additional setup after loading the view.
     }
@@ -45,6 +54,32 @@ class AboutViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Table Methods
+
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        let cellIdentifier = "teamCell"
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        let speaker = self.items[indexPath.row]
+        cell.textLabel?.text = speaker
+        
+        return cell
+        
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
     
 
